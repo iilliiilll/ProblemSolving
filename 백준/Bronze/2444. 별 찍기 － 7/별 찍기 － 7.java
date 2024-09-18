936d346704a4cf6f619021d94746958ca9f4d5a6
soowan <tndhks0708@naver.com>
2024-09-19 00:56:49 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 364 ms, Memory: 20036 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -6,37 +6,29 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = n - i - 1; j > 0; j--) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            for (int k = 1; k <= i * 2 + 1; k++) {
+            for (int k = 1; k <= 2 * i - 1; k++) {
                 System.out.print("*");
             }
 
             System.out.println();
         }
 
-        for (int i = 0; i < 2 * n - 1; i++) {
-            System.out.print("*");
-        }
-
-        System.out.println();
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 1; j < i + 2; j++) {
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            for (int k = (2 * n - 3) - (2 * i); k > 0; k--) {
+            for (int k = 1; k <= 2 * i - 1; k++) {
                 System.out.print("*");
             }
 
             System.out.println();
         }
-
 
         br.close();
     }
