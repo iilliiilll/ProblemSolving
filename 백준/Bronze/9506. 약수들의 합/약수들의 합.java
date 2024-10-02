56b838d76163fb4951d941362b5c3eec2dc8faec
soowan <tndhks0708@naver.com>
2024-10-02 13:02:38 +0900

    [Bronze I] Title: 약수들의 합, Time: 120 ms, Memory: 16184 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n;
+
+        while ((n = Integer.parseInt(br.readLine())) != -1) {
+            int[] arr = new int[n];
+            int j = 0;
+            int sum = 0;
+            int k = 1;
+
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    sum += i;
+                    arr[j++] = i;
+                }
+            }
+
+            if (n == sum) {
+                System.out.print(n + " = " + 1);
+                while (arr[k] != 0) {
+                    System.out.print(" + " + arr[k++]);
+                }
+                System.out.println();
+            } else {
+                System.out.println(n + " is NOT perfect.");
+            }
+
+        }
+
+        br.close();
+    }
+}
