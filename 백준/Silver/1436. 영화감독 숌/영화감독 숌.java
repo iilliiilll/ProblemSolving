1db03ff3214403cc2aef5611b358baa67caaefe3
soowan <tndhks0708@naver.com>
2024-10-08 22:40:51 +0900

    [Silver V] Title: 영화감독 숌, Time: 332 ms, Memory: 87928 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); // n의 범위 : 1 <= n <= 10,000
+        int count = 0; // 6이 적어도 3개 이상 들어가는 경우
+        int i = 0; // 전체 자연수 탐색
+        String s; // i를 문자열로 변환하여 저장
+
+        while (n != count) {
+            s = ++i + "";
+
+            if (s.contains("666")) {
+                count++;
+            }
+        }
+
+        System.out.println(i);
+
+        sc.close();
+    }
+}
