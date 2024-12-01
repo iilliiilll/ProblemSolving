5ec951049dbf3dd945f2afda08b06b0e2dd646d6
soowan <tndhks0708@naver.com>
2024-12-01 09:23:20 +0900

    [Bronze I] Title: 최소공배수, Time: 308 ms, Memory: 15660 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 테스트 케이스의 개수
+        int t = Integer.parseInt(br.readLine());
+
+        // 최대공약수로 최소공배수 구하기
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for (int j = Math.min(a, b); j > 0; j--) {
+                if (a % j == 0 && b % j == 0) {
+                    System.out.println(a * b / j);
+                    break;
+                }
+            }
+        }
+
+        br.close();
+
+    }
+}
