3c70b7f0afd63de3eaf1e7401d0790bc329e1048
soowan <tndhks0708@naver.com>
2024-12-04 14:34:45 +0900

    [Silver III] Title: 소수 구하기, Time: 224 ms, Memory: 20640 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -6,51 +6,41 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        // 자연수 m, n (1 <= m <= n <= 1,000,000)
-        st = new StringTokenizer(br.readLine());
+        // 1 <= m <= n <= 1,000,000
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
 
-        // m이 2보다 작은 경우
-        if (m <= 2) {
-            sb.append(2).append('\n');
-        }
+        // 에라토스테세스의 체
+        int max = 1_000_000;
+        boolean[] prime = new boolean[max + 1];
+        getPrime(prime);
 
-        // 홀수로 변경
-        m = m % 2 == 0 ? m + 1 : m;
-
-        // 소수 구하기
-        while (m <= n) {
-            if (isPrime(m)) {
-                sb.append(m).append('\n');
+        // 소수 찾기
+        for (int i = m; i <= n; i++) {
+            if (prime[i]) {
+                sb.append(i).append('\n');
             }
-
-            m += 2;
         }
 
         System.out.println(sb);
-
-        br.close();
     }
 
-    static boolean isPrime(int n) {
-        if (n == 1) {
-            return false;
-        } else if (n == 2 || n == 3) {
-            return true;
-        } else if (n % 2 == 0) {
-            return false;
+    // 에라토스테네스의 체
+    static void getPrime(boolean[] prime) {
+        // 초기화
+        for (int i = 2; i < prime.length; i++) {
+            prime[i] = true;
         }
 
-        for (int i = 3; i * i <= n; i += 2) {
-            if (n % i == 0) {
-                return false;
+        // 소수가 아니면 false
+        for (int i = 2; i * i < prime.length; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < prime.length; j += i)
+                    prime[j] = false;
             }
         }
-
-        return true;
     }
 }
