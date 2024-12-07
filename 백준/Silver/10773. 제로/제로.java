33191fcc7dd2a11d8f3353921d126d6dc4916213
soowan <tndhks0708@naver.com>
2024-12-07 13:34:15 +0900

    [Silver IV] Title: 제로, Time: 204 ms, Memory: 24044 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        // 스택 생성
+        Stack<Integer> stack = new Stack<>();
+
+        // k ( 1 <= k <= 100,000)
+        int k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
+            // 0 <= n <= 1,000,000
+            int n = Integer.parseInt(br.readLine());
+
+            if (n == 0) {
+                stack.pop();
+            } else {
+                stack.push(n);
+            }
+        }
+
+        int sum = 0;
+        while (!stack.empty()) {
+            sum += stack.pop();
+        }
+
+        System.out.println(sum);
+
+        br.close();
+    }
+}
