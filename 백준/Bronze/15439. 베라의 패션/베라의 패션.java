01e503e2ee99cb6a54064a8282903d314b1e547b
soowan <tndhks0708@naver.com>
2024-12-19 02:23:33 +0900

    [Bronze IV] Title: 베라의 패션, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/15439. 베라의 패션/베라의 패션.java
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -5,9 +5,11 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
 
-        System.out.println(n * n - n);
+        sb.append(n * n - n);
+        System.out.println(sb);
     }
 }
