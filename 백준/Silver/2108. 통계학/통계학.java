4a314cb8c7ceab2ab6f38432f429c7aa085c71ba
soowan <tndhks0708@naver.com>
2024-12-20 23:13:32 +0900

    [Silver III] Title: 통계학, Time: 596 ms, Memory: 64544 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -9,13 +9,13 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         HashMap<Integer, Integer> hm = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         // 1 <= n <= 500,000 (n은 홀수)
         int n = Integer.parseInt(br.readLine());
 
         double sum = 0;
         int[] arr = new int[n];
-
 
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
@@ -25,29 +25,24 @@ public class Main {
             hm.put(num, hm.getOrDefault(num, 0) + 1);
         }
 
-
         // 1. 산술 평균
         double ave = Math.round((sum / n));
-        System.out.println((int) ave);
-
+        sb.append((int) ave).append("\n");
 
         // 2. 중앙값
         Arrays.sort(arr);
-        System.out.println(arr[n / 2]);
-
+        sb.append(arr[n / 2]).append("\n");
 
         // 3. 최빈값
         // 나온 횟수의 최댓값
         int max = -1;
-        for (int i : hm.keySet()) {
-            if (hm.get(i) > max) {
-                max = hm.get(i);
+        for (int i : hm.values()) {
+            if (i > max) {
+                max = i;
             }
         }
 
-        // max가 나온 횟수 구하기
-        ArrayList<Integer> list = new ArrayList<>();
-
+        // 최빈값 저장
         for (int i : hm.keySet()) {
             if (max == hm.get(i)) {
                 list.add(i);
@@ -57,19 +52,18 @@ public class Main {
         Collections.sort(list);
 
         // 1번 나왔다면 바로 출력
-        // 여러 번 나왔다면 두 번째로 작은 값 출력
+        // 그 이상이면 두 번째로 작은 값 출력
         if (list.size() == 1) {
-            System.out.println(list.get(0));
+            sb.append(list.get(0)).append("\n");
         } else {
             list.remove(0);
-            System.out.println(list.get(0));
+            sb.append(list.get(0)).append("\n");
         }
 
-
-
         // 4. 범위
-        System.out.println(arr[n - 1] - arr[0]);
+        sb.append(arr[n - 1] - arr[0]);
 
-
+        // 출력
+        System.out.println(sb);
     }
 }
