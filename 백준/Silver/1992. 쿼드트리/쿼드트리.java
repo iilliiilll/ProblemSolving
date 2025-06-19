3fee118c59817eebb2be7d85192e0e26819acfe0
soowan <tndhks0708@naver.com>
2025-06-19 19:52:49 +0900

    [Silver I] Title: 쿼드트리, Time: 108 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static char[][] video;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // 영상의 크기 n (2, 4, 8, 16, 32, 64)
+        int n = Integer.parseInt(br.readLine());
+
+        // 문자열 (0 or 1)
+        video = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            video[i] = line.toCharArray();
+        }
+
+        divide(0, 0, n);
+
+        System.out.println(sb);
+    }
+
+    static public void divide(int x, int y, int size) {
+
+        if (isEqual(x, y, size)) {
+            if (video[x][y] == '1') {
+                sb.append("1");
+            } else {
+                sb.append("0");
+            }
+
+            return;
+        }
+
+        int newSize = size / 2;
+
+        sb.append("(");
+        divide(x, y, newSize);
+        divide(x, y + newSize, newSize);
+        divide(x + newSize, y, newSize);
+        divide(x + newSize, y + newSize, newSize);
+        sb.append(")");
+
+    }
+
+    static public boolean isEqual(int x, int y, int size) {
+        for (int i = x; i < x + size; i++) {
+            for (int j = y; j < y + size; j++) {
+                if (video[x][y] != video[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
