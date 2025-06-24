0a7b2ca072bc22b012753af78782985c6f52788c
soowan <tndhks0708@naver.com>
2025-06-24 15:11:50 +0900

    [Gold IV] Title: 행렬 제곱, Time: 120 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // 2 <= n <= 5, 1 <= b <= 100,000,000,000
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        int[][] a = new int[n][n];
+
+        // 0 <= 원소 <= 1,000
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                a[i][j] = Integer.parseInt(st.nextToken()) % 1000;
+            }
+        }
+
+        // 행렬 a를 b제곱
+        int[][] result = pow(a, b);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int[][] pow(int[][] a, long b) {
+        if (b == 1) {
+            return a;
+        } else if (b == 2) {
+            return multiply(a, a);
+        }
+
+        int[][] temp = pow(a, b / 2);
+        int[][] matrix = multiply(temp, temp);
+
+        if (b % 2 == 1) {
+            matrix = multiply(matrix, a);
+        }
+
+        return matrix;
+    }
+
+    public static int[][] multiply(int[][] a, int[][] b) {
+        int[][] temp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                temp[i][j] = cal(a, b, i, j);
+            }
+        }
+
+        return temp;
+    }
+
+    public static int cal(int[][] a, int[][] b, int row, int col) {
+        int temp = 0;
+
+        for (int i = 0; i < n; i++) {
+            temp += a[row][i] * b[i][col];
+        }
+
+        return temp % 1000;
+    }
+
+}
