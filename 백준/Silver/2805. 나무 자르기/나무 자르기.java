a278bd6929656b81b5b144e64852f79c2e9e0545
soowan <tndhks0708@naver.com>
2025-07-13 22:11:44 +0900

    [Silver II] Title: 나무 자르기, Time: 476 ms, Memory: 119396 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 class Main {
@@ -11,6 +10,8 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        int start = 0;
+        int end = -1;
 
         // 나무의 수 n (1 <= n <= 1,000,000), 나무의 길이 m (1 <= m <= 2,000,000,000)
         st = new StringTokenizer(br.readLine());
@@ -23,37 +24,29 @@ class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             tree[i] = Integer.parseInt(st.nextToken());
+            end = Math.max(end, tree[i]);
         }
-        Arrays.sort(tree);
 
-        System.out.println(getHeight(m) - 1);
-    }
-
-    private static int getHeight(int height) {
-        int start = 0;
-        int end = tree[tree.length - 1] + 1;
+        end++;
 
         while (start < end) {
             int mid = start + (end - start) / 2;
             long result = 0;
 
             for (int t : tree) {
-                long x = t - mid;
-
-                if (x > 0) {
-                    result += x;
+                if (t - mid > 0) {
+                    result += (t - mid);
                 }
             }
 
-            if (result >= height) {
+            if (result >= m) {
                 start = mid + 1;
             } else {
                 end = mid;
             }
-
         }
 
-        return end;
+        System.out.println(start - 1);
     }
 }
 
