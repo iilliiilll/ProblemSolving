fd1a1b63325ccf047763387008bbc657516dc38a
soowan <tndhks0708@naver.com>
2025-08-07 23:23:04 +0900

    [Silver II] Title: 최대 힙, Time: 280 ms, Memory: 26484 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append('\n');
+            } else {
+                pq.add(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+}
