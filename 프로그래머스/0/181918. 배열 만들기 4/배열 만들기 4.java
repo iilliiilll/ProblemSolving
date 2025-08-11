62c8be318e3c9ba76a874d937931e83c08ce9489
soowan <tndhks0708@naver.com>
2025-08-11 21:41:46 +0900

    [level 0] Title: 배열 만들기 4, Time: 58.29 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(stack.isEmpty() || stack.peek() < arr[i]) {
+                stack.push(arr[i]);
+            } else {
+                stack.pop();
+                i--;
+            }
+        }
+        
+        return stack.stream().mapToInt(i -> i).toArray();
+    }
+}
