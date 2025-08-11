1c4a237fcd4c00a782acd1d9005474663d520c21
soowan <tndhks0708@naver.com>
2025-08-11 21:41:10 +0900

    [level 0] Title: 배열 만들기 2, Time: 2.92 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 1; i < 64; i++) {
+            int num = Integer.parseInt(Integer.toBinaryString(i)) * 5;
+            
+            if(l <= num && num <= r) {
+                list.add(num);
+            }
+        }
+        
+        return list.size() == 0 ? new int[] { -1 } : list.stream().mapToInt(i -> i).toArray();
+    }
+}
