a39ac808e8c2f7ada51004bcbef75b6c804b55f1
soowan <tndhks0708@naver.com>
2025-08-12 22:18:52 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int len = num_list.length;
+        int[] answer = new int[len];
+        
+        for(int i = 0; i < len; i++) {
+            answer[i] = num_list[(n + i) % len];
+        }
+        
+        return answer;
+    }
+}
