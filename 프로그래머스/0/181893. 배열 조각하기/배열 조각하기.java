7c23e1c2d5a5ca08db6b05e8f82b0322c8e4a449
soowan <tndhks0708@naver.com>
2025-08-12 22:00:00 +0900

    [level 0] Title: 배열 조각하기, Time: 9.22 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i : arr) {
+            list.add(i);
+        }
+        
+        for(int i = 0; i < query.length; i++) {
+            if(i % 2 == 0) {
+                list.subList(query[i] + 1, list.size()).clear();
+            } else {
+                list.subList(0, query[i]).clear();
+            }
+        }
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
