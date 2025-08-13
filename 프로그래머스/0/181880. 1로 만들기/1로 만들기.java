1932583852f8c5f50e697053c90c5d99959d0dcd
soowan <tndhks0708@naver.com>
2025-08-13 21:48:39 +0900

    [level 0] Title: 1로 만들기, Time: 0.02 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        for(int i = 0; i < num_list.length; i++) {
+            int n = num_list[i];
+            
+            while(n != 1) {
+                n = n % 2 == 0 ? n / 2 : (n - 1) / 2;
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
