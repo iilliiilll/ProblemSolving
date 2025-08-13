89e67b059e8eab7f139faee8822a0339163cfd5a
soowan <tndhks0708@naver.com>
2025-08-13 22:00:45 +0900

    [level 0] Title: A 강조하기, Time: 0.10 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < myString.length(); i++) {
+            char ch = myString.charAt(i);
+            
+            if(ch == 'a' || ch == 'A') {
+                sb.append("A");
+            } else {
+                sb.append(Character.toLowerCase(ch));
+            }
+        }
+        
+        return sb.toString();
+    }
+}
