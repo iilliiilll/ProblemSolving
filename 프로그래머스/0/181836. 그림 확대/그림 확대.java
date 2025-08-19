46f30e27d61efdb3d3ef7c7a85c17a8b660c68ab
soowan <tndhks0708@naver.com>
2025-08-20 00:02:42 +0900

    [level 0] Title: 그림 확대, Time: 0.30 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,18 +1,9 @@
 class Solution {
     public String[] solution(String[] picture, int k) {
         String[] answer = new String[picture.length * k];
-        String dot = "";
-        String x = "";
-        String s;
-        
-        for(int i = 0; i < k; i++) {
-            dot += ".";
-            x += "x";
-        }
         
         for(int i = 0, j = 0; i < picture.length; i++) {
-            s = picture[i].replace(".", dot);
-            s = s.replace("x", x);
+            String s = picture[i].replace(".", ".".repeat(k)).replace("x", "x".repeat(k));
             
             for(int a = 0; a < k; a++) {
                 answer[j++] = s;
