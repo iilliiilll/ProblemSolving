179fc30dfbcc60be9b55232e04bbb03b8d4a1b63
soowan <tndhks0708@naver.com>
2025-08-20 21:16:39 +0900

    [level 0] Title: l로 만들기, Time: 24.26 ms, Memory: 84.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char ch : myString.toCharArray()) {
+            sb.append(ch < 'l' ? "l" : ch);
+        }
+        
+        return sb.toString();
+    }
+}
