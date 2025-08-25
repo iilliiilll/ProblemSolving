ab145d5e30e3e4a37437c33b0ed39b859b06363c
soowan <tndhks0708@naver.com>
2025-08-25 21:31:15 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 1.17 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,20 +2,12 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        List<Integer> list = new ArrayList<>();
+        my_string = my_string.replaceAll("[a-z]", "");
+        
+        int[] answer = new int[my_string.length()];
         
         for(int i = 0; i < my_string.length(); i++) {
-            char ch = my_string.charAt(i);
-            
-            if('0' <= ch && ch <= '9') {
-                list.add(ch - '0');
-            }
-        }
-        
-        int[] answer = new int[list.size()];
-        
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+            answer[i] = my_string.charAt(i) - '0';
         }
         
         Arrays.sort(answer);
