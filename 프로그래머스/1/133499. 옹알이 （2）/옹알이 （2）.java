97b5d1e2bde8fbcebe93795ccbad165e23252b57
soowan <tndhks0708@naver.com>
2025-09-05 22:50:09 +0900

    [level 1] Title: 옹알이 (2), Time: 2.88 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,44 +1,16 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
-        String[] str = {"aya", "ye", "woo", "ma"};
-        String prefix = "?";
-        boolean flag = false;
         
         for(String b : babbling) {
-            while(true) {
-                if(b.startsWith(prefix)) {
-                    flag = false;
-                    break;
-                }
-                
-                for(String s : str) {
-                    if(b.startsWith(s)) {
-                        prefix = s;
-                        b = b.replaceFirst(s, "");
-                        System.out.println(b);
-                        flag = true;
-                        break;
-                    } else {
-                        flag = false;
-                    }
-                }
-                
-                if(!flag) {
-                    break;
-                }
-                
-                if(b.isEmpty()) {
-                    flag = true;
-                    break;
-                }
+            if(b.contains("ayaaya") || b.contains("yeye") || 
+              b.contains("woowoo") || b.contains("mama")) {
+                continue;
+            } else {
+                answer += b.replace("aya", " ").replace("ye", " ")
+                    .replace("woo", " ").replace("ma", " ")
+                    .replace(" ", "").length() == 0 ? 1 : 0;
             }
-            
-            if(flag) {
-                answer++;
-            }
-            
-            prefix = "?";
         }
         
         return answer;
