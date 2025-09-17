758da8d741794ce7604b5be3527cb2b4ad099667
soowan <tndhks0708@naver.com>
2025-09-17 22:42:51 +0900

    [level 1] Title: 신고 결과 받기, Time: 162.88 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,39 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] reportCount = new int[id_list.length];
-        int[][] arr = new int[id_list.length][id_list.length];
+        Map<String, Integer> idMap = new HashMap<>();
+        Map<String, Set<String>> reportMap = new HashMap<>();
         
-        Map<String, Integer> hm = new HashMap<>();
         for(int i = 0; i < id_list.length; i++) {
-            hm.put(id_list[i], i);
+            idMap.put(id_list[i], i);
+            reportMap.put(id_list[i], new HashSet<>());
         }
         
         StringTokenizer st;
-        for(String str : report) {
-            st = new StringTokenizer(str);
-            int from = hm.get(st.nextToken());
-            int to = hm.get(st.nextToken());
+        for(String r : report) {
+            st = new StringTokenizer(r);
+            String from = st.nextToken();
+            String to = st.nextToken();
             
-            if(arr[from][to] == 0) {
-                arr[from][to] = -1;
-                reportCount[to]++;
-            }
-        }
-        
-        for(int i = 0; i < reportCount.length; i++) {
-            for(int j = 0; j < arr.length; j++) {
-                if(reportCount[i] >= k && arr[j][i] == -1) {
-                    arr[j][i] = 1;
-                }
+            if(!reportMap.get(to).contains(from)) {
+                reportMap.get(to).add(from);
             }
         }
         
         int[] answer = new int[id_list.length];
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr[i].length; j++) {
-                if(arr[i][j] == 1) {
-                    answer[i]++;
+        for(String id : id_list) {
+            if(k <= reportMap.get(id).size()) {
+                for(String r : reportMap.get(id)) {
+                    answer[idMap.get(r)]++;
                 }
             }
         }
