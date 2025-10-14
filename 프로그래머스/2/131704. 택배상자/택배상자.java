cbd35632cb59654b3e12b081163fa4216d3a2ed1
soowan <tndhks0708@naver.com>
2025-10-14 21:46:15 +0900

    [level 2] Title: 택배상자, Time: 57.99 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,38 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        Deque<Integer> x = new ArrayDeque<>();
-        Deque<Integer> y = new ArrayDeque<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+        int idx = 0;
         
-        // x에 order개만큼 넣기
         for(int i = 1; i <= order.length; i++) {
-            x.addLast(i);
-        }
-        
-        // 순회
-        int box = 0;
-        
-        while(true) {
-            // 보조 컨테이너
-            if(!y.isEmpty() && y.getLast() == order[box]) {
-                y.removeLast();
-                box++;
-                continue;
-            }
+            deque.addLast(i);
             
-            // 메인 컨테이너
-            if(!x.isEmpty()) {
-                if(x.getFirst() == order[box]) {
-                    x.removeFirst();
-                    box++;
-                } else {
-                    y.addLast(x.removeFirst());
-                }
-            } else {
-                break;
+            while(!deque.isEmpty() && deque.getLast() == order[idx]) {
+                deque.removeLast();
+                idx++;
             }
         }
         
-        return box;
+        return idx;
     }
 }
