31356104bc2fcf173b724551550f8b286327c5d4
soowan <tndhks0708@naver.com>
2025-10-19 15:29:32 +0900

    [level 2] Title: 소수 찾기, Time: 27.81 ms, Memory: 98.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -41,12 +41,32 @@ class Solution {
             return false;
         }
         
-        for(int i = 2; i * i <= n; i++) {
-            if(n % i == 0) {
+        if (n % 2 == 0) {
+            return n == 2;
+        }
+        
+        if (n % 3 == 0) {
+            return n == 3;
+        }
+        
+        for(int i = 5; i * i <= n; i+=6) {
+            if(n % i == 0 || n % (i + 2) == 0) {
                 return false;
             }
         }
         
         return true;
+        
+//         if(n < 2) {
+//             return false;
+//         }
+        
+//         for(int i = 2; i * i <= n; i++) {
+//             if(n % i == 0) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
     }
 }
