e8e92ff8636830bcc43d4327a778f705ad78168a
soowan <tndhks0708@naver.com>
2025-10-21 23:10:47 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,19 +1,41 @@
 class Solution {
-    static int answer = Integer.MAX_VALUE;
+    // static int answer = Integer.MAX_VALUE;
     
     public int solution(int storey) {
-        dfs(storey, 0);
-        return answer;
-    }
-    
-    private static void dfs(int storey, int count) {
-        if(storey / 10 == 0) {
-            count += Math.min(storey, 11 - storey);
-            answer = Math.min(answer, count);
-            return;
+        // dfs(storey, 0);
+        // return answer;
+        
+        int result = 0;
+        
+        while(storey > 0) {
+            int rest = storey % 10;
+            storey /= 10;
+            
+            if(rest < 5) {
+                result += rest;
+            } else if(rest > 5) {
+                result += (10 - rest);
+                storey++;
+            } else {
+                result += 5;
+                
+                if(storey % 10 >= 5) {
+                    storey++;
+                }
+            }
         }
         
-        dfs(storey / 10, count + storey % 10);
-        dfs(storey / 10 + 1, count + 10 - (storey % 10));
+        return result;
     }
+    
+//     private static void dfs(int storey, int count) {
+//         if(storey / 10 == 0) {
+//             count += Math.min(storey, 11 - storey);
+//             answer = Math.min(answer, count);
+//             return;
+//         }
+        
+//         dfs(storey / 10, count + storey % 10);
+//         dfs(storey / 10 + 1, count + 10 - (storey % 10));
+//     }
 }
