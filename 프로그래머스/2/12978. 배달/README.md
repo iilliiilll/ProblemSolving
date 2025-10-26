1ffa78731bb792a5823cc55b8ef966ce2f71654d
soowan <tndhks0708@naver.com>
2025-10-26 23:28:36 +0900

    [level 2] Title: 배달, Time: 3.93 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 8.25 ms
+메모리: 78.5 MB, 시간: 3.93 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 26일 23:18:30
+2025년 10월 26일 23:28:32
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -5,21 +5,23 @@ class Solution {
     static int[] dist;
     
     public int solution(int N, int[][] road, int K) {
-        // List, dist, Queue 초기화
-        list = new ArrayList[N + 1];
-        dist = new int[N + 1];
+        // List, dist 초기화
+        List<int[]>[] list = new ArrayList[N + 1];
+        int[] dist = new int[N + 1];
         
-        for(int i = 1; i <= N; i++) {
+        for(int i = 1; i <= N ; i++) {
             list[i] = new ArrayList<>();
             dist[i] = Integer.MAX_VALUE;
         }
         
         dist[1] = 0;
         
+        // Queue<[시작, 거리]> 초기화
         Queue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            return a[0] - b[0];
+            return a[1] - b[1];
         });
-        pq.add(new int[] {0, 1});
+
+        pq.add(new int[] {1, 0});
         
         // road 삽입
         for(int i = 0; i < road.length; i++) {
@@ -33,8 +35,8 @@ class Solution {
         
         while(!pq.isEmpty()) {
             int[] temp = pq.poll();
-            int cost = temp[0];
-            int start = temp[1];
+            int start = temp[0];
+            int cost = temp[1];
             
             if(cost > dist[start]) {
                 continue;
@@ -42,11 +44,11 @@ class Solution {
             
             for(int[] next : list[start]) {
                 int nextStart = next[0];
-                int nextCost = cost + next[1];
+                int nextCost = next[1] + cost;
                 
                 if(nextCost < dist[nextStart]) {
                     dist[nextStart] = nextCost;
-                    pq.add(new int[] {nextCost, nextStart});
+                    pq.add(new int[] {nextStart, nextCost});
                 }
             }
         }
