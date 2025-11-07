837161223af6559a445bdbe0d3d16dcc8dded21b
soowan <tndhks0708@naver.com>
2025-11-07 16:40:06 +0900

    [level 2] Title: 우박수열 정적분, Time: 7.61 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.java
@@ -5,19 +5,17 @@ class Solution {
         // 횟수 세기
         List<Integer> list = new ArrayList<>();
         list.add(k);
-        int count = 1;
         
         while(k > 1) {
             if(k % 2 == 0) k /= 2;
             else k = 3 * k + 1;
             
             list.add(k);
-            count++;
         }
         
         // 넓이 구하기
-        double[] s = new double[count];
-        for(int i = 0; i < count - 1; i++) {
+        double[] s = new double[list.size()];
+        for(int i = 0; i < list.size() - 1; i++) {
             s[i] = ((double) list.get(i + 1) + list.get(i)) / 2;
         }
         
@@ -26,7 +24,7 @@ class Solution {
         int idx = 0;
         for(int[] range : ranges) {
             int from = range[0];
-            int to = count - 1 + range[1];
+            int to = list.size() - 1 + range[1];
             
             if(from == to) answer[idx] = 0;
             else if(from > to) answer[idx] = -1;
