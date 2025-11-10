6e448bae34f0603e888e056bd3b6868d797a4163
soowan <tndhks0708@naver.com>
2025-11-10 20:06:21 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.10 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,22 +1,22 @@
 class Solution {
     public long solution(int w, int h) {
-        long count = 0;
+//         long count = 0;
         
-        for(int x = 1; x < Math.min(w, h); x++) {
-            // 높이 내림값 (양수 기울기 사용)
-            count += (long) Math.floor((double) Math.max(w, h) * x / Math.min(w, h));
-        }
+//         for(int x = 1; x < Math.min(w, h); x++) {
+//             // 높이 내림값 (양수 기울기 사용)
+//             count += (long) Math.floor((double) Math.max(w, h) * x / Math.min(w, h));
+//         }
         
-        return count * 2;
+//         return count * 2;
         
-//         long W = (long) w;
-//         long H = (long) h;
+        long W = (long) w;
+        long H = (long) h;
         
-//         return W * H - (W + H - getGcd(w, h));
+        return W * H - (W + H - getGcd(w, h));
     }
     
-    // private static int getGcd(int w, int h) {
-    //     if(h == 0) return w;
-    //     return getGcd(h, w % h);
-    // }
+    private static int getGcd(int w, int h) {
+        if(h == 0) return w;
+        return getGcd(h, w % h);
+    }
 }
