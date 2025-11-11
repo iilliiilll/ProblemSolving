0eca7b744267b377fa73a5b6f8dd34af1c181cac
soowan <tndhks0708@naver.com>
2025-11-11 21:51:52 +0900

    [level 2] Title: 큰 수 만들기, Time: 80.43 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,6 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
+//         StringBuilder sb = new StringBuilder(number);
+        
+//         for (int i = 0; i + 1 < sb.length() && k > 0; i++) {
+//             if(sb.charAt(i) < sb.charAt(i + 1)) {
+//                 sb.deleteCharAt(i);
+//                 i = -1;
+//                 k--;
+//             }
+//         }
+        
+//         if(k != 0)
+//             sb.delete(sb.length() - k, sb.length());
+        
+//         return sb.toString();
+        
         Deque<Character> deque = new ArrayDeque<>();
         int temp = k;
         
@@ -19,9 +34,7 @@ class Solution {
         while(!deque.isEmpty()) {
             sb.append(deque.removeFirst());
             
-            if(sb.length() + k == number.length()) {
-                break;
-            }
+            if(sb.length() + k == number.length()) break;
         }
         
         return sb.toString();
