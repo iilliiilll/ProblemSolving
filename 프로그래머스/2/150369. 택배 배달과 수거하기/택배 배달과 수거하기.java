b400df2630961d134fbdbec7d092ce60cba59bcc
soowan <tndhks0708@naver.com>
2025-11-18 22:16:48 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 32.56 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,43 +1,39 @@
-import java.util.*;
-
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
         
-        Deque<Integer> deli = new ArrayDeque<>();
-        Deque<Integer> pick = new ArrayDeque<>();
-
-        // deque 채우기
-        for(int d : deliveries) deli.addLast(d);
-        for(int p : pickups) pick.addLast(p);
+        int dPoint = n - 1;
+        int pPoint = n - 1;
 
         // 모두 빌 때까지 반복
-        while(!deli.isEmpty() || !pick.isEmpty()) {
+        while(dPoint >= 0 || pPoint >= 0) {
             // 0은 모두 빼기
-            while(!deli.isEmpty() && deli.getLast() == 0) deli.removeLast();
-            while(!pick.isEmpty() && pick.getLast() == 0) pick.removeLast();
+            while(dPoint >= 0 && deliveries[dPoint] == 0) dPoint--;
+            while(pPoint >= 0 && pickups[pPoint] == 0) pPoint--;
+            
+            if(dPoint < 0 && pPoint < 0) break;
 
             // 마지막 집의 거리의 최댓값까지 가야함
-            answer += Math.max(deli.size(), pick.size());
+            answer += Math.max(dPoint, pPoint) + 1;
 
             // cap개 배달, 수거
             int d = cap;
-            while(!deli.isEmpty() && d > 0) {
-                if(deli.getLast() > d) {
-                    deli.addLast(deli.removeLast() - d);
+            while(dPoint >= 0 && d > 0) {
+                if(deliveries[dPoint] > d) {
+                    deliveries[dPoint] -= d;
                     d = 0;
                 } else {
-                    d -= deli.removeLast();
+                    d -= deliveries[dPoint--];
                 }
             }
 
             int p = cap;
-            while(!pick.isEmpty() && p > 0) {
-                if(pick.getLast() > p) {
-                    pick.addLast(pick.removeLast() - p);
+            while(pPoint >= 0 && p > 0) {
+                if(pickups[pPoint] > p) {
+                    pickups[pPoint] -= p;
                     p = 0;
                 } else {
-                    p -= pick.removeLast();
+                    p -= pickups[pPoint--];
                 }
             }
         }
