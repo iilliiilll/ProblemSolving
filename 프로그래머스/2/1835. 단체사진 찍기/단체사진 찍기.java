0d086526e556aee2ddee157d1f02978e6ffb573c
soowan <tndhks0708@naver.com>
2025-11-21 18:19:43 +0900

    [level 2] Title: 단체사진 찍기, Time: 1435.06 ms, Memory: 380 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -8,15 +8,38 @@ class Solution {
     public int solution(int n, String[] data) {
         // 모든 경우의 수 만들면서 가지치기
         answer = 0;
-        dfs(0, "", data);
+        dfs("", data);
         return answer;
     }
     
-    private static void dfs(int count, String str, String[] data) {
-        if(count == 8) {
-            if(isGood(str, data)) {
-                answer++;
+    private static void dfs(String str, String[] data) {
+        if(str.length() == 8) {
+            boolean b = false;
+            
+            for(String d : data) {
+                int diff = Math.abs(str.indexOf(d.charAt(2)) - str.indexOf(d.charAt(0))) - 1;
+
+                if(d.charAt(3) == '>') {
+                    if(diff <= d.charAt(4) - '0') {
+                        b = false;
+                        break;
+                    }
+                } else if(d.charAt(3) == '<') {
+                    if(diff >= d.charAt(4) - '0') {
+                        b = false;
+                        break;
+                    }
+                } else {
+                    if(diff != d.charAt(4) - '0') {
+                        b = false;
+                        break;
+                    }
+                }
+                
+                b = true;
             }
+            
+            if(b) answer++;
             
             return;
         }
@@ -24,39 +47,9 @@ class Solution {
         for(int i = 0; i < 8; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                dfs(count + 1, str + kakao[i], data);
+                dfs(str + kakao[i], data);
                 visited[i] = false;
             }
         }
-    }
-    
-    // str이 조건에 맞는지?
-    private static boolean isGood(String str, String[] data) {
-        for(String d : data) {
-            char ch1 = d.charAt(0);
-            char ch2 = d.charAt(2);
-            char compare = d.charAt(3);
-            int dist = d.charAt(4) - '0';
-            
-            int start = str.indexOf(ch1);
-            int end = str.indexOf(ch2);
-            int diff = Math.abs(end - start) - 1;
-            
-            if(compare == '>') {
-                if(diff <= dist) {
-                    return false;
-                }
-            } else if(compare == '<') {
-                if(diff >= dist) {
-                    return false;
-                }
-            } else {
-                if(diff != dist) {
-                    return false;
-                }
-            }
-        }
-        
-        return true;
     }
 }
