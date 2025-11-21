37b89a9a142d5925d6913dda16c20b9be5918222
soowan <tndhks0708@naver.com>
2025-11-21 18:26:51 +0900

    [level 2] Title: 단체사진 찍기, Time: 1058.61 ms, Memory: 407 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,36 +1,44 @@
 import java.util.*;
 
 class Solution {
-    static boolean[] visited = new boolean[8];
     static char[] kakao = new char[] {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
+    static char[] result = new char[8];
+    static boolean[] visited = new boolean[8];
     static int answer = 0;
     
     public int solution(int n, String[] data) {
         // 모든 경우의 수 만들면서 가지치기
         answer = 0;
-        dfs("", data);
+        dfs(0, data);
         return answer;
     }
     
-    private static void dfs(String str, String[] data) {
-        if(str.length() == 8) {
-            boolean b = false;
+    private static void dfs(int count, String[] data) {
+        if(count == 8) {
+            // str 만들기
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < 8; i++) {
+                sb.append(result[i]);
+            }
+            String str = sb.toString();
             
+            boolean b = false;
             for(String d : data) {
                 int diff = Math.abs(str.indexOf(d.charAt(2)) - str.indexOf(d.charAt(0))) - 1;
+                int dist = d.charAt(4) - '0';
 
                 if(d.charAt(3) == '>') {
-                    if(diff <= d.charAt(4) - '0') {
+                    if(diff <= dist) {
                         b = false;
                         break;
                     }
                 } else if(d.charAt(3) == '<') {
-                    if(diff >= d.charAt(4) - '0') {
+                    if(diff >= dist) {
                         b = false;
                         break;
                     }
                 } else {
-                    if(diff != d.charAt(4) - '0') {
+                    if(diff != dist) {
                         b = false;
                         break;
                     }
@@ -47,7 +55,8 @@ class Solution {
         for(int i = 0; i < 8; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                dfs(str + kakao[i], data);
+                result[count] = kakao[i];
+                dfs(count + 1, data);
                 visited[i] = false;
             }
         }
