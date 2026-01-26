1c4eb073ff0c92b786f01f2ee57624eaa14b374f
soowan <tndhks0708@naver.com>
2026-01-26 18:09:53 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.73 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,65 +1,33 @@
-import java.util.*;
-
 class Solution {
-    /*
-    n x m 격자 미로: (x, y) -> (r, c)
-        1. 격자 안에서만 이동
-        2. 이동 거리는 총 k. (중복 방문 가능)
-        3. 탈출 경로 -> 문자열: 사전 순으로 가장 빠른 경로로 탈출 (d -> l -> r -> u)
-            l: 왼쪽, r: 오른쪽, u: 위, d: 아래
-        return 탈출 경로. 안되면 "impossible"
-    2 <= n, m <= 50
-    1 <= x, r <= n
-    1 <= y, c <= m
-    1 <= k <= 2,500
-    풀이
-        bfs? dp? -> 이동하면서 사전 순으로 계속 교체?
-            d, l, r, u 순으로 계속 이동하다가 현재 위치와 (r, c)와의 거리가 남은 k와 같으면 bfs
-    */
-    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        // impossible 구하기
-        if (k % 2 != (getDistance(x, y, r, c) % 2)) return "impossible";
+        // impossible
+        if (getDistance(x, y, r, c) > k || (k - getDistance(x, y, r, c)) % 2 == 1) {
+            return "impossible";
+        }
         
-        // d -> l -> r, l 번갈아 (현재 위치와 (r, c)와의 거리가 k와 같을 때까지)
         StringBuilder sb = new StringBuilder();
         
-        while (x < n && getDistance(x + 1, y, r, c) < k - 1) {
-            x++;
-            k--;
-            sb.append("d");
-        }
-        
-        while (y > 1 && getDistance(x, y - 1, r, c) < k - 1) {
-            y--;
-            k--;
-            sb.append("l");
-        }
-        
-        boolean dir = false;
-        
-        while (true) {
-            if (!dir && y < m && getDistance(x, y + 1, r, c) < k - 1) {
-                y++;
-                sb.append("r");
-            } else if (dir && 1 < y && getDistance(x, y - 1, r, c) < k - 1) {
-                y--;
+        // d -> l -> r -> u
+        while (k-- > 0) {
+            int down = x + 1;
+            int left = y - 1;
+            int right = y + 1;
+            int up = x - 1;
+            
+            if (down <= n && getDistance(down, y, r, c) <= k) {
+                x = down;
+                sb.append("d");
+            } else if (left >= 1 && getDistance(x, left, r, c) <= k) {
+                y = left;
                 sb.append("l");
-            } else {
-                break;
+            } else if (right <= m && getDistance(x, right, r, c) <= k) {
+                y = right;
+                sb.append("r");
+            } else if (up >= 1 && getDistance(up, y, r, c) <= k) {
+                x = up;
+                sb.append("u");
             }
-
-            dir = !dir;
-            k--;
         }
-        
-        // (x, y)에서 (r, c)까지 이동하기: d -> l -> r -> u 순
-        if (x < r) sb.append("d".repeat(r - x));
-        
-        if (y > c) sb.append("l".repeat(y - c));
-        else if (y < c) sb.append("r".repeat(c - y));
-        
-        if (x > r) sb.append("u".repeat(x - r));
         
         return sb.toString();
     }
