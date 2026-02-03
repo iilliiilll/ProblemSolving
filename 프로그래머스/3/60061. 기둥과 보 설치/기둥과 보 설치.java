d95a261e0d81bdffc51eac93909d23a3475fb6f9
soowan <tndhks0708@naver.com>
2026-02-03 17:54:30 +0900

    [level 3] Title: 기둥과 보 설치, Time: 2.33 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -1,100 +1,79 @@
 import java.util.*;
 
 class Solution {
+    static boolean[][] pillar;
+    static boolean[][] beam;
+    static int N;
+    
     public int[][] solution(int n, int[][] build_frame) {
-        Set<String> set = new HashSet<>();
+        N = n;
+        pillar = new boolean[n + 1][n + 1];
+        beam = new boolean[n + 1][n + 1];
         
         for (int[] frame : build_frame) {
             int x = frame[0];
             int y = frame[1];
             int struct = frame[2];
-            boolean install = frame[3] == 0 ? false : true;
+            int install = frame[3];
             
-            // 설치할 수 있으면 set에 추가, 삭제할 수 있으면 set에서 제거
-            if (install) {
-                if (struct == 0 && canInstall(set, n, x, y, struct)) {
-                    set.add(x + "," + y + ",0");
-                } else if (struct == 1 && canInstall(set, n, x, y, struct)) {
-                    set.add(x + "," + y + ",1");
+            if (install == 1) { // 설치
+                if (canInstall(x, y, struct)) {
+                    if (struct == 0) pillar[x][y] = true;
+                    else beam[x][y] = true;
                 }
-            } else {
-                if (struct == 0) {
-                    Set<String> temp = new HashSet<>(set);
-                    temp.remove(x + "," + y + ",0");
-                    if (possible(temp, n)) set.remove(x + "," + y + ",0");
-                } else if (struct == 1) {
-                    Set<String> temp = new HashSet<>(set);
-                    temp.remove(x + "," + y + ",1");
-                    if (possible(temp, n)) set.remove(x + "," + y + ",1");
+            } else { // 삭제
+                if (struct == 0) pillar[x][y] = false;
+                else beam[x][y] = false;
+                
+                if (!isValid(x, y)) {
+                    if (struct == 0) pillar[x][y] = true;
+                    else beam[x][y] = true;
                 }
             }
         }
         
-        // return
-        List<String> list = new ArrayList<>(set);
-        list.sort((a, b) -> {
-            StringTokenizer st = new StringTokenizer(a, ",");
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int st1 = Integer.parseInt(st.nextToken());
-            
-            st = new StringTokenizer(b, ",");
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int st2 = Integer.parseInt(st.nextToken());
-            
-            if (x1 != x2) return x1 - x2;
-            else if (y1 != y2) return y1 - y2;
-            else return st1 - st2;
-        });
-        
-        int[][] answer = new int[list.size()][3];
-        for(int i = 0; i < answer.length; i++) {
-            StringTokenizer st = new StringTokenizer(list.get(i), ",");
-            answer[i][0] = Integer.parseInt(st.nextToken());
-            answer[i][1] = Integer.parseInt(st.nextToken());
-            answer[i][2] = Integer.parseInt(st.nextToken());
-        }
-        
-        return answer;
-    }
-
-    
-    static boolean canInstall(Set<String> set, int n, int x, int y, int struct) {
-        if (struct == 0) {
-            // 바닥 위
-            if (y == 0) return true;
-            
-            // 다른 기둥 위
-            int nx = x;
-            int ny = y - 1;
-            if (ny >= 0 && set.contains(nx + "," + ny + ",0")) return true;
-            
-            // 보의 한쪽 끝 부분 위
-            if (set.contains(x + "," + y + ",1") || (x - 1 >= 0 && set.contains((x - 1) + "," + y + ",1"))) return true;
-        } else {
-            // 한쪽 끝 부분이 기둥 위
-            if (set.contains(x + "," + (y - 1) + ",0") || set.contains((x + 1) + "," + (y - 1) + ",0")) return true;
-            
-            // 양쪽 끝 부분이 다른 보와 동시에 연결
-            if (set.contains((x - 1) + "," + y + ",1") && set.contains((x + 1) + "," + y + ",1")) return true;
-        }
-        
-        return false;
+        return makeAnswer();
     }
     
-    static boolean possible(Set<String> temp, int n) {
-        for (String t : temp) {
-            StringTokenizer st = new StringTokenizer(t, ",");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            int struct = Integer.parseInt(st.nextToken());
-            
-            if (!canInstall(temp, n, x, y, struct)) {
-                return false;
+    static boolean canInstall(int x, int y, int struct) {
+        if (struct == 0) { // 기둥
+            return y == 0 || (y > 0 && pillar[x][y - 1]) || beam[x][y]|| (x > 0 && beam[x - 1][y]);
+        } else { // 보
+            return (y > 0 && pillar[x][y - 1]) || (y > 0 && x + 1 <= N && pillar[x + 1][y - 1]) || (x > 0 && x + 1 <= N && beam[x - 1][y] && beam[x + 1][y]);
+        }
+    }
+    
+    static boolean isValid(int x, int y) {
+        for (int i = x - 1; i <= x + 1; i++) {
+            for (int j = y; j <= y + 1; j++) {
+                if (i < 0 || j < 0 || i > N || j > N) continue;
+                
+                if (pillar[i][j] && !canInstall(i, j, 0)) return false;
+                if (beam[i][j] && !canInstall(i, j, 1)) return false;
             }
         }
         
         return true;
+    }
+    
+    static int[][] makeAnswer() {
+        List<int[]> list = new ArrayList<>();
+        
+        for (int x = 0; x <= N; x++) {
+            for (int y = 0; y <= N; y++) {
+                if (pillar[x][y]) list.add(new int[] {x, y, 0});
+                if (beam[x][y]) list.add(new int[] {x, y, 1});
+            }
+        }
+        
+        list.sort((a, b) -> {
+            if (a[0] != b[0]) return a[0] - b[0];
+            if (a[1] != b[1]) return a[1] - b[1];
+            return a[2] - b[2];
+        });
+        
+        int[][] answer = new int[list.size()][3];
+        for (int i = 0; i < list.size(); i++) answer[i] = list.get(i);
+        return answer;
     }
 }
